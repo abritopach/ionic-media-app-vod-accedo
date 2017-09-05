39c7ae4f7f33b13a9271bff26b6b8cc11d568975
Adrián Brito <adrbrpa1988@gmail.com>
2017-09-05 12:03:18 +0200

    Fullscreen video

--- src/components/video-carousel/video-carousel.ts
+++ src/components/video-carousel/video-carousel.ts
@@ -13,6 +13,7 @@ import { Component, Input } from '@angular/core';
 export class VideoCarouselComponent {
 
     videos: any;
+    historyVideos: any = [];
 
     @Input()
     set videosData(videos: Array<Object>) {
@@ -22,6 +23,93 @@ export class VideoCarouselComponent {
 
     constructor() {
         console.log('Hello VideoCarouselComponent Component');
+        this.historyVideos = localStorage.getItem('historyVideos') ? JSON.parse(localStorage.getItem('historyVideos')) : [];
+    }
+
+    handleSelectedVideo(video) {
+        console.log("handleSelectedVideo");
+        console.log(video);
+
+        let elem = document.getElementById(video.id);
+
+        this.handleFullScreenVideo(video, elem);
+    }
+
+    handleFullScreenVideo(selectedVideo, elem) {
+
+        // Reset video.
+        elem.pause();
+        elem.currentTime = 0;
+        elem.load();
+
+        if (elem != null) {
+
+            if (elem.requestFullscreen) {
+                elem.requestFullscreen();
+            } else if (elem.webkitRequestFullscreen) {
+                elem.webkitRequestFullscreen();
+            } else if (elem.mozRequestFullScreen) {
+                elem.mozRequestFullScreen();
+            } else if (elem.msRequestFullscreen) {
+                elem.msRequestFullscreen();
+            }
+
+            // Show loading animation.
+
+            let playPromise = elem.play();
+
+            if (playPromise !== undefined) {
+
+                /*
+                var exists = false;
+                for (var i = 0; i < this.historyVideos.length; i++) {
+                    if (this.historyVideos[i].id === selectedVideo.id) {
+                        exists = true;
+                        this.historyVideos[i].countViews = this.historyVideos[i].countViews + 1;
+                        localStorage.setItem('historyVideos', JSON.stringify(this.historyVideos));
+                        break;
+                    }
+                }
+
+                if (exists === false) {
+
+                    selectedVideo.countViews = 1;
+                    this.historyVideos.push(selectedVideo);
+                    localStorage.setItem('historyVideos', JSON.stringify(this.historyVideos));
+                }
+                 */
+
+
+                playPromise.then(_ => {
+                    // Automatic playback started!
+                    // Show playing UI.
+                })
+                    .catch(error => {
+                        // Auto-play was prevented
+                        // Show paused UI.
+                        console.log(error);
+                    });
+            }
+
+            // Video Ended.
+            document.getElementById(selectedVideo.id).addEventListener('ended', () => {
+                elem.webkitExitFullScreen();
+            }, false)
+
+            // Full Screen Change.
+            document.addEventListener('webkitfullscreenchange', () => {
+
+                let isFullScreen = document.fullscreenElement || document.webkitIsFullScreen;
+
+                if (!isFullScreen) {
+                    elem.pause();
+                    elem.currentTime = 0;
+                    elem.load();
+                }
+
+            }, false);
+
+        }
     }
 
 }
